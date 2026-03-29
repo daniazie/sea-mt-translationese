30fc347e6f71c0fdc7893378268a341e4cd6c5ab
daniazie <dania.moriazi01@gmail.com>
2026-03-29 09:22:41 +0000

    update files

--- scripts/sft.sh
+++ scripts/sft.sh
@@ -4,22 +4,22 @@ IS_VL=$3
 
 export PYTORCH_ALLOC_CONF="expandable_segments:True"
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-CUDA_VISIBLE_DEVICES=2 uv run accelerate launch --use_deepspeed --config_file configs/accelerate_single_gpu_config.yaml --main_process_port 0 src/train/sft.py \
+CUDA_VISIBLE_DEVICES=2 uv run accelerate launch --config_file configs/accelerate_single_gpu_config.yaml --main_process_port 0 src/train/sft.py \
     --model ${MODEL} ${IS_VL} \
     --dataset_name_or_path ${DATASET} \
-    --fp16 \
+    --bf16 \
     --output_dir /data/${USER}/sea-mt/models \
     --per_device_train_batch_size=4 \
-    --gradient_accumulation_steps=32 \
+    --gradient_accumulation_steps=16 \
     --per_device_eval_batch_size=4 \
-    --llm_int8_enable_fp32_cpu_offload \
-    --eval_accumulation_step=32 \
+    --eval_accumulation_step=16 \
     --eval_steps=5 \
     --warmup_ratio=0.1 \
     --evaluation_strategy='steps' \
+    --save_strategy best \
     --num_train_epochs=5 \
-    --weight_decay=0.1 \
-    --learning_rate=2e-4 \
+    --weight_decay=0.01 \
+    --learning_rate=5e-5 \
     --lr_scheduler='cosine' \
     --max_seq_length=2048 \
     --logging_steps=5 \
@@ -27,12 +27,10 @@ CUDA_VISIBLE_DEVICES=2 uv run accelerate launch --use_deepspeed --config_file co
     --save_total_limit 2 \
     --report_to="wandb" \
     --loss_type='dft' \
-    --prompt_completion_format \
-    --use_liger_kernel \
+    --completion_only_loss \
     --lora_r=64 \
     --lora_alpha=256 \
     --lora_dropout=0.0 \
     --lora_target_modules q_proj,k_proj,v_proj,o_proj \
-    --lora_bias="none" \
-    --activation_offloading
-exit
+    --lora_bias="none" 
+
