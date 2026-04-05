eec63bbb5f795abc4b1d152e450f7b2ddf3a2de0
daniazie <dania.moriazi01@gmail.com>
2026-04-05 14:53:12 +0900

    commit

--- t-index/src/t_index.py
+++ t-index/src/t_index.py
@@ -1,19 +1,27 @@
 import os
-import sys
+import yaml
 import argparse
-import tqdm
-from pathlib import Path
+import pandas as pd
 from functools import partial
+from glob import glob
+from types import SimpleNamespace
 
-from typing_extensions import Literal
-
+from dataclasses import asdict
+import torch
+from datasets import Dataset
 from torch.utils.data import DataLoader
-from datasets import load_dataset, Dataset
-from transformers import AutoConfig, AutoTokenizer, AutoModelForCausalLM, PreTrainedModel, PreTrainedTokenizerBase, PretrainedConfig
-
+import torch.nn as nn
+import torch.nn.functional as F
 import torch
 
-class TranslationeseIndex:
+from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorWithPadding, PreTrainedModel, PreTrainedTokenizerBase
+from trl.trainer.sft_trainer import DataCollatorForLanguageModeling
+from tqdm import tqdm
+
+
+from utils import TIndexArgs, encode, preprocess_dataset, tokenize_fn, format_messages
+        
+class TranslationeseEval:
     """
     Wrapper for Translationese-Index by Liu et al., (2025).
     Original TranslationeseIndex code can be found here: https://github.com/yikang0131/TranslationeseIndex
@@ -26,261 +34,95 @@ class TranslationeseIndex:
         tokenizer ([`str`] or [`PreTrainedTokenizerBase`] or [`None`]):
             Processing class to be used to tokenize samples. Uses the model's tokenizer if None is passed.
     """
-    def __init__(self, positive_model: str | PreTrainedModel, negative_model: str | PreTrainedModel, tokenizer: str | PreTrainedTokenizerBase | None = None):
-        if tokenizer is None and isinstance(positive_model, str):
-            tokenizer = AutoTokenizer.from_pretrained(positive_model)
-        if isinstance(positive_model, str):
-            positive_model = AutoModelForCausalLM.from_pretrained(positive_model, dtype=torch.bfloat16)
-        if isinstance(negative_model, str):
-            negative_model = AutoModelForCausalLM.from_pretrained(negative_model, dtype=torch.bfloat16)
+    def __init__(self, model: str| PreTrainedModel, tokenizer: str | PreTrainedTokenizerBase = None, data_collator=None, use_dpo=False, args: dict | TIndexArgs | None = None):
+        if isinstance(model, str):
+            self.model = AutoModelForCausalLM.from_pretrained(model, dtype=torch.bfloat16)
+            if tokenizer is None:
+                self.tokenizer = AutoTokenizer.from_pretrained(model, padding_side='left', add_eos_token=True)
+        if isinstance(tokenizer, str):
+            self.tokenizer = AutoTokenizer.from_pretrained(tokenizer, padding_side='left', add_eos_token=True)
+        else:
+            self.model = model
+            self.tokenizer = tokenizer
+
+        if args is None:
+            args = TIndexArgs()
         
-        if tokenizer is None:
-            tokenizer = positive_model.name_or_path
-            tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+        if isinstance(args, dict):
+            args = TIndexArgs(**args)
+
+        self.args = asdict(args)
+        self.batch_size = self.args.pop("batch_size")
+        self.use_dpo = use_dpo
+
+        if data_collator is None:
+            try: 
+                pad_token_id = tokenizer.pad_token_id
+            except:
+                pad_token_id = tokenizer.pad_token_type_id
+            self.data_collator = DataCollatorForLanguageModeling(pad_token_id, max_length=args.max_length)
+        else:
+            self.data_collator = data_collator
+
+    def compute_log_lklh(self, model: nn.Module, model_inputs: dict):
+        outputs = model(**model_inputs)
+        logits: torch.Tensor = outputs.logits[:, :-1]
+        input_ids: torch.Tensor = model_inputs["input_ids"][:, 1:]
+        response_mask: torch.Tensor = model_inputs['attention_mask'][:, 1:].bool()
         
-        self.positive_model = positive_model
-        self.negative_model = negative_model
-        self.tokenizer = tokenizer
-
-    def format_func(self, examples, lang, labels=[], pairwise=False):
-        prompt_pmt1ix = f"Translate the following text to {lang}.\n\n"
-        srcs = [example for example in examples['src']]
-        mts = [example for example in examples['mt']]
-
-        mt_data = []
-        for src, mt in zip(srcs, mts):
-            messages = [
-                {"role": "user", "content": prompt_pmt1ix + src},
-                {"role": "assistant", "content": mt}
-            ]
-            mt_data.append(messages)
-        
-        if labels:
-            return {"messages": mt_data, "labels": labels}
-        
-        if pairwise:
-            mt1_data = []
-            assert examples.get('mt1'), "Dataset must contain a `mt1` column for pairwise evaluation."
-            mt1 = [example for example in examples['mt1']]
-            for src, mt1 in zip(srcs, mt1):
-                messages = [
-                    {"role": "user", "content": prompt_pmt1ix + src},
-                    {"role": "assistant", "content": mt1}
-                ]
-                mt1_data.append(messages)
-            
-            return {"mt1_messages": mt1_data, "mt2_messages": mt_data}
-
-        return {"messages": messages}
-    
-    def tokenize_func(self, examples, tokenizer, max_length):
-        user_input_len = [
-            len(item) for item in tokenizer.apply_chat_template(
-                [m[:1] for m in examples['messages']],
-                add_generation_prompt=True
-            )
-        ]
-
-        concatinated_input_len = [
-            len(item) for item in tokenizer.apply_chat_template(
-                examples['messages']
-            )
-        ]
-
-        input_ids = tokenizer.apply_chat_template(
-            examples['messages'],
-            padding='max_length',
-            truncation=True,
-            max_length=max_length,
-            return_tensors='pt'
-        )
-
-        mask = torch.ones(input_ids.shape, type=torch.bool)
-        for i in range(len(examples['messages'])):
-            user_len = min(user_input_len[i], max_length)
-            concat_len = min(concatinated_input_len[i], max_length)
-
-            mask[i, :user_len] = False
-            mask[i, concat_len:] = False
-
-            assert mask[i].sum().item() == concat_len - user_len
-
-        return {"input_ids": input_ids, "score_mask": mask}
-    
-    @torch.no_grad()
-    def get_log_lklh(self, model, input_ids, score_mask):
-        logits: torch.Tensor = model(input_ids).logits[:, :-1]
-        input_ids = input_ids[:, 1:]
-        score_mask: torch.Tensor = score_mask[:, 1:]
         log_lklh = logits.log_softmax(dim=-1)
-        log_lklh = log_lklh.gather(-1, input_ids.unsqueeze(-1)).unsqueeze(-1)
-        log_lklh = log_lklh.masked_fill(~score_mask, 0)
-        mean_log_lklh = log_lklh.sum(dim=1) / score_mask.sum(dim=1)
-        return mean_log_lklh
-    
-    def compute_pairwise(self, data: Dataset | dict | list, lang: str, max_length: int = 1024, batch_size: int = 32, num_proc: int | None = None, device: str = "cuda"):
-        positive_model = self.positive_model
-        negative_model = self.negative_model
+        log_lklh = log_lklh.gather(-1, input_ids.unsqueeze(-1)).squeeze(-1)
+        log_lklh = log_lklh.masked_fill(~response_mask, 0)
+        return log_lklh.sum(dim=1) / response_mask.sum(dim=1)
 
-        positive_model.eval()
-        negative_model.eval()
 
-        if isinstance(data, dict):
-            data = Dataset.from_dict(data)
-        elif isinstance(data, list):
-            data = Dataset.from_list(data)
+    def forward(self, dataset, model, tokenizer, data_collator=None):
+        args = self.args
+        dataset = preprocess_dataset(dataset, eos_token=tokenizer.eos_token, **args)
+        if args.get("apply_chat_template") == True:
+            dataset = dataset.map(format_messages)
+        tokenize_func = partial(tokenize_fn, tokenizer=tokenizer, max_length=args['max_length'], apply_chat_template=args['apply_chat_template'])
+  
+        # dataloader = DataLoader(
+        #     dataset,
+        #     batch_size=self.batch_size,
+        #     shuffle=False,
+        #     collate_fn=self.data_collator,
+        # )
+        
+        # dataset = dataset.batch(batch_size=self.batch_size)
+        dataset.set_transform(encode, output_all_columns=True)
+        results = []
+        with torch.no_grad():
+            rewards = []
+            for data in tqdm(dataset, desc="Evaluating...", total=len(dataset)):
+                print(data)
+                input_ids = torch.tensor(data['input_ids']).to(model.device)
+                mask_label = "attention_mask" if data_collator else "completion_mask"
+                completion_mask = torch.tensor(data[mask_label]).to(model.device)
 
-        format_fn = partial(self.format_func, lang=lang)
+                model_inputs = {
+                    "input_ids": input_ids,
+                    "attention_mask": completion_mask,
+                    "return_output": True
+                }
 
-        data = data.map(format_fn, batched=True)
-
-        tokenize_fn = partial(self.tokenize_func, tokenizer=self.tokenizer, max_length=max_length)
-        tokenized_data = data.map(
-            tokenize_fn,
-            batched=True,
-            batch_size=batch_size,
-            num_proc=num_proc,
-            load_from_cache_file=False
-        )
-
-        llrs = []
-
-        tokenized_data.set_format(type="torch", columns=["input_ids", "score_mask"], output_all_columns=True)
-        for i in tqdm(range(0, len(tokenized_data), batch_size), desc="Evaluating..."):
-            batch = tokenized_data[i:i+batch_size]
-            inputs = {k: batch[k].to(device) for k in ["input_ids", "score_mask"]}
-            log_lklh_positive = self.get_log_lklh(model=positive_model, **inputs)
-            log_lklh_negative = self.get_log_lklh(model=negative_model, **inputs)
-
-            llr = log_lklh_positive - log_lklh_negative
-            llrs.extend(llr.tolist())
-
-        assert len(llrs) == data.num_rows
-
-        mean_llr = torch.tensor(llrs).sum(dim=-1) / len(llrs)
-        mean_score = mean_llr.sigmoid()
-
-        return {"translationese_score": mean_score.item(), "naturalness_score": 1 - mean_score.item()}
-
-    def compute_rewards(self, lklh_natural_refs, lklh_translationese_refs, lklh_natural_preds, lklh_translationese_preds):
-        ref_probs = (lklh_natural_refs - lklh_translationese_refs).sigmoid()
-        pred_probs = (lklh_natural_preds - lklh_translationese_preds).sigmoid()
-
-        return (ref_probs - pred_probs).item()
-
-    def compute_penalties(self, llr_refs, llr_preds):
-        ref_probs = llr_refs.sigmoid()
-        pred_probs = llr_preds.sigmoid()
-
-        return (pred_probs - ref_probs).item()
-
-    def compute_pairwise(self, data: Dataset | dict | list, lang: str, max_length: int = 1024, batch_size: int = 32, num_proc: int | None = None, device: str = "cuda", return_rewards=False, return_penalties=False):
-        translationese_model = self.positive_model
-        natural_model = self.negative_model
-
-        translationese_model.eval()
-        natural_model.eval()
-
-        format_fn = partial(self.format_func, lang=lang, pairwise=True)
-        data = data.map(
-            format_fn,
-            batched=True
-        )
-
-        tokenize_fn = partial(self.tokenize_func, tokenizer=self.tokenizer, max_length=max_length)
-
-        tokenized_mt1 = data.select_columns("mt1_messages").map(
-            tokenize_fn,
-            batched=True,
-            batch_size=batch_size,
-            num_proc=num_proc,
-            load_from_cache_file=False
-        )
-
-        tokenized_mt2 = data.select_columns("mt2_messages").map(
-            tokenize_fn,
-            batched=True,
-            batch_size=batch_size,
-            num_proc=num_proc,
-            load_from_cache_file=False
-        )
-
-        tokenized_mt1.set_format(type='torch', columns=['input_ids', 'score_mask'], output_all_columns=True)
-        tokenized_mt2.set_format(type='torch', columns=['input_ids', 'score_mask'], output_all_columns=True)
-
-        results = {
-                "pred_label": [],
-            }
-
-        for i in range(0, len(data), batch_size):
-            mt1_batch = tokenized_mt1[i:i+batch_size]
-            mt2_batch = tokenized_mt2[i:i+batch_size]
-
-            mt1_inputs = {k: mt1_batch[k].to(device) for k in ['input_ids', 'score_mask']}
-            mt2_inputs = {k: mt2_batch[k].to(device) for k in ['input_ids', 'score_mask']}
-
-            lklh_natural_mt1 = self.get_log_lklh(natural_model, **mt1_inputs)
-            lklh_translationese_mt1 = self.get_log_lklh(translationese_model, **mt1_inputs)
-
-            lklh_natural_mt2s = self.get_log_lklh(natural_model, **mt2_inputs)
-            lklh_translationese_mt2s = self.get_log_lklh(translationese_model, **mt2_inputs)
-
-            llr_mt1 = lklh_translationese_mt1 - lklh_natural_mt1
-            llr_mt2 = lklh_translationese_mt2s - lklh_natural_mt2s
-
-            reward_func_inputs = {
-                "lklh_natural_refs": lklh_natural_mt1,
-                "lklh_translationese_refs": lklh_translationese_mt1,
-                "lklh_natural_preds": lklh_natural_mt2s,
-                "lklh_translationese_preds": lklh_translationese_mt2s
-            }
-
-            if return_rewards:
-                rewards = self.compute_rewards(**reward_func_inputs)
-            if return_penalties:
-                penalties = self.compute_penalties(llr_refs=llr_mt1, llr_preds=llr_mt2)
-
-            for mt1, mt2 in zip(llr_mt1, llr_mt2):
-                if mt1 > mt2:
-                    results['preds'].append("mt1")
-                else:
-                    results['preds'].append("mt2")
-
-            mt1_translationese_score = torch.tensor(llr_mt1).sum(dim=-1) / len(llr_mt1)
-            mt2_translationese_score = torch.tensor(llr_mt2).sum(dim=-1) / len(llr_mt2)
-
-            results['mt1_translationese_score'] = mt1_translationese_score.sigmoid().item()
-            results['mt1_natural_score'] = 1 - mt1_translationese_score.sigmoid().item()
-
-            results['mt2_translationese_score'] = mt2_translationese_score.sigmoid().item()
-            results['mt2_natural_score'] = 1 - mt2_translationese_score.sigmoid().item()
-
-            if return_rewards:
-                results['rewards'] = rewards
-            if return_penalties:
-                results['penalties'] = penalties
+                reward = self.compute_log_lklh(model, model_inputs)
+                reward = reward.flatten().cpu().tolist()
+                rewards.append({
+                    "file_path": data['file_path'],
+                    "source": data['prompt'],
+                    "translation": data['completion'],
+                    "label": data['label'],
+                    "log_lklh": reward
+                })
 
         return results
 
-    def __call__(self, data: Dataset | dict | list, lang: str, max_length: int = 1024, batch_size: int = 32, num_proc: int | None = None, device: str = "cuda", mode: Literal["pointwise", "pairwise"] = "pointwise", return_rewards=False, return_penalties=False):
-        if mode == 'pointwise':
-            return self.compute_pointwise(
-                data=data,
-                lang=lang,
-                max_length=max_length,
-                batch_size=batch_size,
-                num_proc=num_proc,
-                device=device
-            )
-        
-        elif mode == 'pairwise':
-            return self.compute_pairwise(
-                data=data,
-                lang=lang,
-                max_length=max_length,
-                batch_size=batch_size,
-                num_proc=num_proc,
-                device=device,
-                return_rewards=return_rewards,
-                return_penalties=return_penalties
-            )
+    def __call__(self, data, **args):
+        return self.forward(
+            data,
+            model=self.model,
+            tokenizer=self.tokenizer,
+            data_collator=self.data_collator,
+        )
--- t-index/src/utils.py
+++ t-index/src/utils.py
@@ -1,7 +1,174 @@
-from datasets import Dataset
-import json
+from datasets import Dataset, interleave_datasets
+from dataclasses import dataclass, field
+from functools import partial
 
-def format_messages(examples, is_vl):
+from transformers import PreTrainedTokenizerBase
+from transformers.utils import PaddingStrategy
+from typing_extensions import List, Dict, Union
+import torch
+import json
+import numpy as np
+
+
+@dataclass
+class TranslationeseIndexArgs:
+    prompt_key: str
+    completion_key: str | None = None
+    positive_key: str | None = None
+    negative_key: str | None = None
+    prompt_template: str | None = None
+    batch_size: int = 32
+    max_length: int = 1024
+    padding: bool | str | PaddingStrategy = True
+    apply_chat_template: bool = False
+    device: str = 'cpu'
+
+def prepare_dataset(examples, prompt_template, **kwargs):
+    prompts = []
+    completions = [completion for completion in examples['completion']]
+
+    for prompt in examples['prompt']:
+        prompts.append(prompt_template.format(input=prompt))
+
+    samples = {
+        "prompt": prompts,
+        "completion": completions
+    }
+
+    if examples.get('label'):
+        samples['label'] = [example for example in examples['label']]
+    
+    if examples.get('file_path'):
+        samples['file_path'] = [example for example in examples['file_path']]
+
+    return samples
+
+def preprocess_dataset(dataset: Dataset, prompt_key, prompt_template, positive_key, negative_key, completion_key, **kwargs):
+    if positive_key and negative_key:
+        neg_dataset = dataset.select_columns(["file_path", prompt_key, negative_key])
+        neg_dataset = neg_dataset.rename_columns({prompt_key: "prompt", negative_key: "completion"})
+        neg_dataset = neg_dataset.add_column("label", column=[0] * neg_dataset.num_rows)
+        pos_dataset = dataset.select_columns(["file_path", prompt_key, positive_key])
+        pos_dataset = pos_dataset.rename_columns({prompt_key: "prompt", positive_key: "completion"})
+        pos_dataset = pos_dataset.add_column("label", column=[1] * pos_dataset.num_rows)
+
+        ds = interleave_datasets([pos_dataset, neg_dataset])
+    else:
+        ds = dataset.rename_columns({prompt_key: "prompt", completion_key: "completion"})
+
+    prep_dataset = partial(prepare_dataset, prompt_template=prompt_template, **kwargs)
+    return ds.map(prep_dataset, batched=True)
+
+def _tokenize(inputs, tokenizer: PreTrainedTokenizerBase, apply_chat_template: bool = False, **kwargs):
+    if apply_chat_template:
+        return tokenizer.apply_chat_template(
+            inputs,
+            **kwargs
+        )
+    else:
+        return tokenizer(inputs)
+
+def tokenize_fn(example, tokenizer: PreTrainedTokenizerBase, max_length, apply_chat_template: bool = False):
+    output = {}
+
+    if apply_chat_template:
+        prompt_ids = _tokenize(
+        example['prompt'],
+        tokenizer=tokenizer,
+        apply_chat_template=apply_chat_template,
+        tokenize=True,
+        add_generation_prompt=True,
+        return_dict=True,
+        return_tensors='pt'
+    )['input_ids']
+
+        prompt_completion_processed = _tokenize(
+            example['prompt'] + example['completion'],
+            tokenizer=tokenizer,
+            apply_chat_template=apply_chat_template,
+            tokenize=True,
+            max_length=max_length,
+            return_dict=True,
+            return_tensors='pt'
+        )
+
+    else:
+        prompt_ids = _tokenize(
+            example['prompt'],
+            return_tensors='pt'
+        )['input_ids']
+
+        prompt_completion_processed = _tokenize(
+            example['prompt'] + example['completion'] + tokenizer.eos_token,
+            return_tensors='pt'
+        )
+        
+    prompt_completion_ids = prompt_completion_processed["input_ids"]
+    if not prompt_completion_ids[:len(prompt_ids) - 1] == prompt_ids[:-1]:
+        print(prompt_completion_ids[:len(prompt_ids)])
+
+    completion_mask = [0] * len(prompt_ids) + [1] * (len(prompt_completion_ids) - len(prompt_ids))
+    output['file_path'] = example['file_path']
+    output['prompt'] = example['prompt']
+    output['completion'] = example['completion']
+    if example.get('label'):
+        output['label'] = example['label']
+    output['input_ids'] = prompt_completion_ids
+    output['completion_mask'] = completion_mask
+    
+    return output
+
+def encode(batch, tokenizer: PreTrainedTokenizerBase, max_length, apply_chat_template: bool = False):
+    if apply_chat_template:
+        prompt_ids = _tokenize(
+            batch['prompt'],
+            tokenizer=tokenizer,
+            apply_chat_template=apply_chat_template,
+            add_generation_prompt=True,
+            tokenize=True,
+            return_dict=True,
+            return_tensors='pt'
+        )['input_ids']
+
+        prompt_completion_processed = _tokenize(
+            batch['prompt'] + batch['completion'],
+            tokenizer=tokenizer,
+            apply_chat_template=apply_chat_template,
+            tokenize=True,
+            max_length=max_length,
+            return_dict=True,
+            return_tensors='pt'
+        )
+    else:
+        prompt_ids = _tokenize(
+            batch['prompt'],
+            tokenizer=tokenizer,
+            apply_chat_template=apply_chat_template,
+            return_tensors='pt'
+        )['input_ids']
+
+        prompt_completion_processed = _tokenize(
+            batch['prompt'] + batch['completion'] + tokenizer.eos_token,
+            tokenizer=tokenizer,
+            apply_chat_template=apply_chat_template,
+            return_tensors='pt'
+        )
+
+    prompt_completion_ids = prompt_completion_processed["input_ids"]
+    if not prompt_completion_ids[:len(prompt_ids) - 1] == prompt_ids[:-1]:
+        print(prompt_completion_ids[:len(prompt_ids)])
+
+    completion_mask = torch.tensor([0] * len(prompt_ids) + [1] * (len(prompt_completion_ids) - len(prompt_ids)))
+
+    return {
+        "input_ids": prompt_completion_ids,
+        "attention_mask": completion_mask
+    }
+
+TIndexArgs = TranslationeseIndexArgs
+
+
+def format_messages_for_preference(examples, is_vl):
     srcs = [example for example in examples['src']]
     refs = [example for example in examples['ref']]
     mts = [example for example in examples['mt']]
@@ -40,3 +207,13 @@ def format_messages(examples, is_vl):
         completions_rejected.append(rejected)
 
     return {"prompt": prompt, "chosen": completions_chosen, "rejected": completions_rejected}
+
+def format_messages(example):
+    prompt = [{"role": "user", "content": example['prompt']}]
+    completion = [{"role": "assistant", "content": example['completion']}]
+    return {
+        "file_path": example['file_path'],
+        "prompt": prompt,
+        "completion": completion,
+        "label": example['label']
+    }
